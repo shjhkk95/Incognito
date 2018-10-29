e8879310124f23954eeab2acf29b3e1c522471f2
nspirrello <nicholaspirrello@gmail.com>
2018-10-29 19:06:03 -0400

    Add string manipulation functions for password generation

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import requests
 import password_generator
+from bs4 import BeautifulSoup
+import socket
 
 def web_crawler():
     print('Hello Web Crawler')
     requests.get_request()
     requests.post_request()
-    password_generator.convert_to_leetspeak()
-    password_generator.reverse_password()
 
+    print(password_generator.generate_password_list(["leet", "code", "hacker"]))
 
 if __name__ == '__main__':
     web_crawler()
--- password_generator.py
+++ password_generator.py
@@ -1,5 +1,22 @@
-def convert_to_leetspeak():
-    print('Password to leetspeak')
+def leetspeak(password):
+    intab = "aeltoAELTO"
+    outtab = "4317043170"
+    trans = str.maketrans(intab, outtab)
+    return password.translate(trans)
 
-def reverse_password():
-    print('Reversed password')
+def reverse(password):
+    return password[::-1]
+
+def uppercase(password):
+    return str.upper(password)
+
+def lowercase(password):
+    return str.lower(password)
+
+def generate_password_list(passwords):
+    size = len(passwords)
+    password_set = set(passwords)
+    for i in range(size):
+        password_set.update([leetspeak(passwords[i]), reverse(passwords[i]), 
+            uppercase(passwords[i]), lowercase(passwords[i])])
+    return list(password_set)
