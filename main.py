9ff493c8633b77a2df4aad7189b1f68f0f4bfac3
nspirrello <nicholaspirrello@gmail.com>
2018-10-31 20:10:55 -0400

    Start BFS implementation

--- main.py
+++ main.py
@@ -13,22 +13,11 @@ def web_crawler():
     print(password_generator.generate_password_list(["leet", "code", "hacker"]))
 
 def crawl_bfs(initial_link):
-    # Make a GET request to initial_link
+    #do bfs
+    return 0
 
-    # Get list of links and keywords on the webpage
-
-    # Add keywords to
-
-    # Check if the links are in the visited set
-
-    # If they are not in the set, add them to the dequeue
-
-    # Remove link from the head of the dequeue and 
-    # call crawl_bfs
-
-def crawl_dfs():
-    # Do a dfs
-
+def crawl_dfs(initial_link):
+    return 0
 
 if __name__ == '__main__':
     web_crawler()
