8065af5e0df9e3c8c48cced0cde6c9833493560d
Brandon Chen <brandon.chen@stonybrook.edu>
2018-11-29 22:57:01 -0500

    Added URL argument and refactored more code

--- main.py
+++ main.py
@@ -31,14 +31,12 @@ def add_duplicate_url(url, seen):
 
 def process_node(start_url, node, header_dict, forms, keywords, seen, add_next, counter):
     if counter.count >= counter.page_max:
-        print('Crawling aborted: The maximum number of pages of ' + str(counter.page_max) + ' has been reached!')
         return
-
-    print('Depth ' + str(node.depth) + ': Processing: ' + node.url)
     
     # Make GET request to the current URL      
     html_doc = get_request(node.url, header_dict)
     if html_doc is not None:
+        print('Depth ' + str(node.depth) + ': Processing: ' + node.url)
         counter.count += 1
 
         parser = HTMLParser(html_doc)
@@ -68,49 +66,57 @@ def process_node(start_url, node, header_dict, forms, keywords, seen, add_next, 
                     add_next(url) 
 
 def crawl_bfs(start_url, header_dict, counter):
-    print('Starting Breadth-First Crawling')
-    print('-' * 50)
     forms = set()
     keywords = set()
     seen = set()
     queue = collections.deque()
-    
-    # Add start URL to the queue and set of seen URLs
-    start_node = Node(start_url, 0)
-    queue.append(start_node)
-    seen.add(start_node.url)
-    add_duplicate_url(start_node.url, seen)
 
-    # While queue is not empty
-    while queue:
-        node = queue.popleft()
-        add_to_queue = lambda url: queue.append(Node(url, node.depth + 1))  
-        process_node(start_url, node, header_dict, forms, keywords, seen, add_to_queue, counter)
+    if counter.count >= counter.page_max:
+        print('\nCrawling Aborted: The maximum number of pages of ' + str(counter.page_max) + ' has been reached!')
+    else:
+        print('\nStarting Breadth-First Crawling')
+        print('-' * 50)
+        
+        # Add start URL to the queue and set of seen URLs
+        start_node = Node(start_url, 0)
+        queue.append(start_node)
+        seen.add(start_node.url)
+        add_duplicate_url(start_node.url, seen)
 
-        if counter.count >= counter.page_max:
-            return keywords, forms, seen
+        # While queue is not empty
+        while queue:
+            node = queue.popleft()
+            add_to_queue = lambda url: queue.append(Node(url, node.depth + 1))  
+            process_node(start_url, node, header_dict, forms, keywords, seen, add_to_queue, counter)
+
+            if counter.count >= counter.page_max:
+                print('\nCrawling Aborted: The maximum number of pages of ' + str(counter.page_max) + ' has been reached!')
+                return keywords, forms, seen
         
     return keywords, forms, seen
 
 def crawl_dfs(start_url, header_dict, counter):
-    """ Wrapper function to initialize first call to recursive DFS crawl function """
-    
-    print('Starting Depth-First Crawling')
-    print('-' * 50)    
+    """ Wrapper function to initialize first call to recursive DFS crawl function """ 
     forms = set()
     keywords = set()
     seen = set()
 
-    start_node = Node(start_url, 0)
-    # Add start URL to the set of seen URLs
-    seen.add(start_node.url)
-    add_duplicate_url(start_node.url, seen)
-    
-    _crawl_dfs(start_url, start_node, header_dict, forms, keywords, seen, counter)
+    if counter.count >= counter.page_max:
+        print('\nCrawling Aborted: The maximum number of pages of ' + str(counter.page_max) + ' has been reached!')
+    else:
+        print('\nStarting Depth-First Crawling')
+        print('-' * 50)    
+        start_node = Node(start_url, 0)
+        # Add start URL to the set of seen URLs
+        seen.add(start_node.url)
+        add_duplicate_url(start_node.url, seen)
+        
+        _crawl_dfs(start_url, start_node, header_dict, forms, keywords, seen, counter)
     return keywords, forms, seen
 
 def _crawl_dfs(start_url, node, header_dict, forms, keywords, seen, counter):
     if counter.count >= counter.page_max:
+        print('\nCrawling Aborted: The maximum number of pages of ' + str(counter.page_max) + ' has been reached!')
         return
     add_to_stack = lambda url: _crawl_dfs(start_url, Node(url, node.depth + 1), header_dict, forms, keywords, seen, counter)
     process_node(start_url, node, header_dict, forms, keywords, seen, add_to_stack, counter) 
@@ -129,21 +135,24 @@ def get_subdomains(url):
     if url_subdomain in subdomains:
         host = host.replace(url_subdomain + '.', '')
 
-    return {subdomain + '.' + host for subdomain in subdomains}
+    return {subdomain + '.' + host for subdomain in subdomains if subdomain != url_subdomain}
 
 def crawl_subdomains(bfs, url, header_dict, counter):
+    print('\nCrawling Subdomains')
+    print('-' * 50)    
     for subdomain in get_subdomains(url):
         try:
             conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
             # Test whether subdomain exists 
             conn.connect((subdomain, 80))
             subdomain = reformat_url(subdomain)
+            print('\nCrawling Subdomain: ' + subdomain)
             if bfs:
                 crawl_bfs(subdomain, header_dict, counter)
             else:
                 crawl_dfs(subdomain, header_dict, counter)            
         except socket.gaierror:
-            print('Subdomain ' + subdomain + ' was not found.')
+            print('\nSubdomain ' + subdomain + ' was not found.')
         finally:
             conn.close()    
             
@@ -165,7 +174,7 @@ def brute_force(user, keywords, forms, user_agent):
             response = post_request(form, post, login)
             
             if get_status(response) >= 500:
-                print('Hold on, too many failed attempts! Waiting for the server to accept more login requests...')
+                print('\nHold on, too many failed attempts! Waiting for the server to accept more login requests...\n')
             
             # Continually retry logging in if there is a server error (too many failed attempts)
             while get_status(response) >= 500:
@@ -182,64 +191,49 @@ def brute_force(user, keywords, forms, user_agent):
         print('Ran out of passwords! Bruteforce failed!')
           
 def main():
-    parser = argparse.ArgumentParser(description= 'Arguments to proceed web-crawling')
-    parser.add_argument('--u', nargs = 1, type = str, help = 'USERNAME')
-    parser.add_argument('--maxdepth', nargs = 1, type = int, help = 'MAX DEPTH TO CRAWL')
-    parser.add_argument('--maxpages', nargs = 1, type = int, help = 'MAX PAGES TO CRAWL')
-    parser.add_argument('--mode', nargs = 1, type = str, help='MODE OF CRAWLING : \'bfs\' OR \'dfs\'')
-    parser.add_argument('--useragent', nargs = 1, type = str, help = 'USER AGENT')
+    parser = argparse.ArgumentParser(description='Arguments to proceed web-crawling')
+    parser.add_argument('-url', nargs=1, type=str, help='URL', required=True)
+    parser.add_argument('--username', nargs=1, type=str, help='Username')
+    parser.add_argument('--maxdepth', nargs=1, type=int, help='Max Depth to Crawl')
+    parser.add_argument('--maxpages', nargs=1, type=int, help='Max Pages to Crawl')
+    parser.add_argument('--mode', nargs=1, type=str, help='Mode of Crawling : "bfs" or "dfs"')
+    parser.add_argument('--useragent', nargs=1, type=str, help='User Agent')
     args = parser.parse_args()
-    username, mode = '',''
-    maxdepth, maxpage = 0,0
-    header_dict = {}
-    if (args.u ==None):
-        username = 'admin'
-    else :
-        username = args.u.pop()
-    if (args.maxdepth == None):
-        maxdepth = 10
-    else :
-        maxdepth = args.maxdepth.pop()
-    if (args.maxpages == None):
-        maxpage = 10
-    else :
-        maxpage = args.maxpages.pop()
-    if (not(args.mode == 'bfs' or args.mode == 'dfs')):
-        mode = 'bfs'
-    else :
-        mode = args.mode.pop()
-    if (args.useragent != None):
-        header_dict = {'User Agent': args.useragent.pop()}
-    useragent = header_dict.get('User Agent')
-    useragent = useragent if useragent != None else 'Default value set (Left to be determined by connection)'
-    print('Initiating web crawl with following configuration\n Algorithm: {}, Username: {}, Max Pages: {}, Max Page Depth: {}, User Agent: {}'
-    .format(mode, username, maxpage, maxdepth, useragent))
 
-    url = 'http://3.16.219.26/'
-    url = reformat_url(url)
-    page_counter = Counter(0, maxdepth, maxpage)
+    modes = [['bfs'], ['dfs']]
 
-    useragent = header_dict.get('User Agent')
-    useragent = useragent if useragent != None else ''
+    url = reformat_url(args.url.pop())
+    username = 'admin' if args.username is None else args.username
+    max_depth = 10 if args.maxdepth is None else args.maxdepth.pop()
+    max_pages = 10 if args.maxpages is None else args.maxpages.pop()
+    mode = args.mode.pop() if args.mode in modes else 'bfs'
+
+    if (args.useragent is not None):
+        user_agent = args.useragent.pop()
+        header_dict = {'User-Agent': user_agent}
+    else:
+        user_agent = 'N/A'
+        header_dict = {}
+
+    print('Initiating web crawl with following configuration')
+    print('Algorithm: {}, Username: {}, Max Pages: {}, Max Page Depth: {}, User Agent: {}'.format(mode, username, max_pages, max_depth, user_agent))
+
+    page_counter = Counter(0, max_depth, max_pages)
 
     if mode == 'bfs':
         """
         BFS
         """
         keywords, forms, seen = crawl_bfs(url, header_dict, page_counter)
-        print('\n\nKeywords:', keywords, '\n\nForm:', forms, '\n\nSeen:', seen, sep=' ')
-        brute_force(username, keywords, forms, useragent)
+        crawl_subdomains(True, url, header_dict, page_counter)
     elif mode == 'dfs':
         """
         DFS
         """
         keywords, forms, seen = crawl_dfs(url, header_dict, page_counter)
-        print('\n\nKeywords:', keywords, '\n\nForm:', forms, '\n\nSeen:', seen, sep=' ')
-        brute_force(username, keywords, forms, useragent)
-    else:
-        print('\'{}\' is not supported as a search algorithm. Please use \'bfs\' or \'dfs\'!'.format(mode))
-        return
-
+        crawl_subdomains(False, url, header_dict, page_counter)
+    
+    brute_force(username, keywords, forms, user_agent)
     print('\n\n\n', get_subdomains(url))
 
 if __name__ == '__main__':
